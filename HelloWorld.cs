ffd6ed923ca25aada63aabd4cc8694c6f191ec44
paramlokare <147266783+paramlokare@users.noreply.github.com>
2023-10-27 14:22:29 +0530

    commit

---
+++ HelloWorld.cs
@@ -0,0 +1,15 @@
+using System;
+
+class HelloWorld
+{
+    static void Main()
+    {
+#if DebugConfig
+        Console.WriteLine("WE ARE IN THE DEBUG CONFIGURATION");
+#endif
+
+        Console.WriteLine("Hello, world!");
+		
+		Console.WriteLine("Build Tool Is Working fine....");
+    }
+}
